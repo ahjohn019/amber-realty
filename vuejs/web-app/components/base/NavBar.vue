51f10fb4383780c24e4ffa0ae72f35395123fec6
ahjohn019 <yewrui@hotmail.com>
2023-12-22 17:57:08 +0800

    02-22122023
    - Add link for nav bar

--- vuejs/web-app/components/base/NavBar.vue
+++ vuejs/web-app/components/base/NavBar.vue
@@ -2,7 +2,9 @@
     <div>
         <q-toolbar class="text-white shadow-2 bg-primary" style="height: 75px">
             <div class="col-6 col-md-2">
-                <q-btn flat label="Amber Realty" />
+                <router-link to="/" exact>
+                    <q-btn flat label="Amber Realty" />
+                </router-link>
             </div>
 
             <div class="col text-right nav-bar-dropdown">
@@ -10,13 +12,15 @@
             </div>
 
             <div class="col-8 text-center nav-bar-content">
-                <q-btn-toggle
-                    v-model="model"
-                    flat
-                    stretch
-                    toggle-color="yellow"
-                    :options="options"
-                />
+                <router-link
+                    v-for="(option, key) in options"
+                    :key="key"
+                    :to="option.to"
+                    :exact="option.exact"
+                    :active-class="option.activeClass"
+                >
+                    <q-btn flat :label="option.label" />
+                </router-link>
             </div>
             <div class="col-2 text-right nav-bar-content">
                 <q-btn
@@ -41,9 +45,27 @@ export default {
             model: ref('home'),
 
             options: [
-                { label: 'Home', value: 'home' },
-                { label: 'About Us', value: 'about_us' },
-                { label: 'Property', value: 'property' },
+                {
+                    label: 'Home',
+                    value: 'home',
+                    to: '/',
+                    exact: true,
+                    activeClass: 'active-link',
+                },
+                {
+                    label: 'About Us',
+                    value: 'about_us',
+                    to: '/about',
+                    exact: false,
+                    activeClass: 'active-link',
+                },
+                {
+                    label: 'Property',
+                    value: 'property',
+                    to: '/property',
+                    exact: false,
+                    activeClass: 'active-link',
+                },
             ],
         };
     },
@@ -57,6 +79,11 @@ export default {
 
 .nav-bar-dropdown {
     display: none;
+}
+
+.active-link {
+    color: #ffd278;
+    font-weight: bold;
 }
 
 @media (max-width: 768px) {
--- vuejs/web-app/components/property/ListComponent.vue
+++ vuejs/web-app/components/property/ListComponent.vue
@@ -1,53 +1,55 @@
 <template>
     <div class="col-12 row">
         <div class="col-12 col-md-6 col-lg-3 p-2">
-            <q-card class="my-card">
-                <div class="relative">
-                    <img src="https://cdn.quasar.dev/img/mountains.jpg" />
-                    <div
-                        class="absolute top-4 left-5 bg-secondary font-bold px-6 py-2 rounded"
-                    >
-                        For Sale
-                    </div>
-                </div>
-
-                <q-card-section class="row gap-2">
-                    <div class="col-12 text-lg">RM 120,000</div>
-                    <div class="col-12 text-xl font-bold">
-                        Stunning Oceanfront Condo
-                    </div>
-                    <div class="col-12 text-subtitle2">12 Miami Beach, FL</div>
-                </q-card-section>
-
-                <q-card-section class="q-pt-none">
-                    <div class="row">
-                        <div class="col-12 col-md-4">
-                            <q-icon
-                                name="arrow_circle_right"
-                                size="22px"
-                                class="align-middle"
-                            />
-                            <span class="align-middle"> 2 Beds </span>
-                        </div>
-                        <div class="col-12 col-md-4">
-                            <q-icon
-                                name="arrow_circle_right"
-                                size="22px"
-                                class="align-middle"
-                            />
-                            <span class="align-middle"> 3 Bath </span>
-                        </div>
-                        <div class="col-12 col-md-4">
-                            <q-icon
-                                name="arrow_circle_right"
-                                size="22px"
-                                class="align-middle"
-                            />
-                            <span class="align-middle"> 900 sqft </span>
+            <router-link to="/property/details/2">
+                <q-card class="my-card">
+                    <div class="relative">
+                        <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+                        <div
+                            class="absolute top-4 left-5 bg-secondary font-bold px-6 py-2 rounded"
+                        >
+                            For Sale
                         </div>
                     </div>
-                </q-card-section>
-            </q-card>
+                    <q-card-section class="row gap-2">
+                        <div class="col-12 text-lg">RM 120,000</div>
+                        <div class="col-12 text-xl font-bold">
+                            Stunning Oceanfront Condo
+                        </div>
+                        <div class="col-12 text-subtitle2">
+                            12 Miami Beach, FL
+                        </div>
+                    </q-card-section>
+                    <q-card-section class="q-pt-none">
+                        <div class="row">
+                            <div class="col-12 col-md-4">
+                                <q-icon
+                                    name="arrow_circle_right"
+                                    size="22px"
+                                    class="align-middle"
+                                />
+                                <span class="align-middle"> 2 Beds </span>
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <q-icon
+                                    name="arrow_circle_right"
+                                    size="22px"
+                                    class="align-middle"
+                                />
+                                <span class="align-middle"> 3 Bath </span>
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <q-icon
+                                    name="arrow_circle_right"
+                                    size="22px"
+                                    class="align-middle"
+                                />
+                                <span class="align-middle"> 900 sqft </span>
+                            </div>
+                        </div>
+                    </q-card-section>
+                </q-card>
+            </router-link>
         </div>
         <div class="col-12 col-md-6 col-lg-3 p-2">
             <q-card class="my-card">
