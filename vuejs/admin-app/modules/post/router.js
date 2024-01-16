bcd12a5fbb2c36ddf38162f41a4f519ed7672225
ahjohn019 <yewrui@hotmail.com>
2024-01-17 01:08:06 +0800

    17012024
    - Remove postPageView

--- vuejs/admin-app/modules/post/router.js
+++ vuejs/admin-app/modules/post/router.js
@@ -1,6 +1,5 @@
 const PostPage = () => import('./views/PostPage.vue');
 const PostPageList = () => import('./views/PostPageList.vue');
 const PostPageForm = () => import('./views/PostPageForm.vue');
-const PostPageView = () => import('./views/PostPageView.vue');
 
-export { PostPage, PostPageList, PostPageForm, PostPageView };
+export { PostPage, PostPageList, PostPageForm };
