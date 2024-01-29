9d83cc4b47aa318e2bf77931ed13b80e6e5cc387
ahjohn019 <yewrui@hotmail.com>
2024-01-30 00:46:26 +0800

    30012024
    - Add Mission Statement

--- vuejs/web-app/modules/main/views/MainPage.vue
+++ vuejs/web-app/modules/main/views/MainPage.vue
@@ -4,6 +4,7 @@
             <q-layout class="mt-20">
                 <HeaderContainer data-aos="fade-up" />
                 <PropertiesContainer data-aos="fade-left" />
+                <MissionStatement data-aos="fade-right" />
                 <SocialProof data-aos="fade-right" />
                 <CallToAction data-aos="fade-left" />
             </q-layout>
@@ -17,6 +18,7 @@ import PropertiesContainer from '@web/components/main/PropertiesContainer.vue';
 import SocialProof from '@web/components/main/SocialProof.vue';
 import CallToAction from '@web/components/main/CallToAction.vue';
 import BaseLayout from '@web/modules/layout/BaseLayout.vue';
+import MissionStatement from '@web/components/main/MissionStatement.vue';
 
 export default {
     components: {
@@ -25,6 +27,7 @@ export default {
         PropertiesContainer,
         SocialProof,
         CallToAction,
+        MissionStatement,
     },
 };
 </script>
