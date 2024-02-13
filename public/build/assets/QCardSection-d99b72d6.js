45469ff0576e1ced26bb43e87880c463dcae4f8a
ahjohn019 <yewrui@hotmail.com>
2024-02-14 00:18:35 +0800

    #npm run build

--- public/build/assets/QCardSection-d99b72d6.js
+++
@@ -1,1 +0,0 @@
-import{l as e,E as r}from"./ckeditor-d5cd79ed.js";import{c as n,e as s}from"./vm-8a8b0649.js";const l=n({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:o}){const a=e(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>r(t.tag,{class:a.value},s(o.default))}});export{l as Q};
