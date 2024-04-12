9a580fa5ce8561bdcce61abec607dd20aa63c482
ahjohn019 <yewrui@hotmail.com>
2024-04-12 23:55:52 +0800

    12042024
    - adjust latest properties section mobile responsive size
    - adjust of font size with mobile responsive size
    - adjust of gap sections

--- vuejs/web-app/components/main/PropertiesContainer.vue
+++ vuejs/web-app/components/main/PropertiesContainer.vue
@@ -1,21 +1,20 @@
 <template>
     <div class="bg-primary">
         <div
-            class="row container mx-auto gap-10"
-            :class="$q.screen.lt.sm ? 'py-12 px-4' : 'py-24 px-12'"
+            class="row container mx-auto gap-10 py-12 px-2 w-full md:w-[650px] lg:w-[990px] xl:w-[1200px]"
         >
             <div
-                class="col-12 px-2 row items-center gap-2 md:gap-0"
+                class="col-12 px-2 row items-center gap-2"
                 :class="$q.screen.lt.sm ? 'justify-center' : 'justify-between'"
             >
                 <div
-                    class="col col-auto text-3xl font-bold text-white"
+                    class="col col-auto text-xl md:text-4xl font-bold text-white"
                     :class="$q.screen.lt.md ? 'text-center' : ''"
                 >
                     Explore Our Latest Properties
                 </div>
                 <div
-                    class="col col-auto font-bold text-white"
+                    class="col col-auto font-bold text-white text-sm"
                     style="text-wrap: nowrap"
                 >
                     <router-link :to="{ name: 'property.list' }"
