851ddaca2a26fffd7b068940d14b70af1a8a6158
ahjohn019 <yewrui@hotmail.com>
2024-05-20 20:02:41 +0800

    #npm run build

---
+++ public/build/assets/AboutUs-c901031d.js
@@ -0,0 +1,33 @@
+import{Q as l,a as f}from"./QBreadcrumbs-f9605adc.js";import{Q as r}from"./QImg-21d7b9bb.js";import{_ as v}from"./BaseLayout-11f66661.js";import{z as _,r as c,l as y,m as i,n as b,q as d,y as e,A as s,W as o,E as u,u as p,T as x,F as w}from"./ckeditor-568c0a64.js";import"./vm-f21f9386.js";import"./QBtn-05999df9.js";import"./QToolbar-5253e172.js";import"./QMenu-1bc4daf5.js";import"./use-tick-0d48cbe6.js";import"./QSeparator-da84d5f5.js";import"./ClosePopup-b4152ee9.js";import"./dayjs.min-67c2dead.js";import"./QSpace-b0ae8a1e.js";import"./QCardSection-a12a20b2.js";import"./QCard-e8f25c2d.js";import"./QDialog-fffb21e1.js";const A={components:{BaseLayout:v},setup(){const t=c({}),a=c([]);return a.value=[{content:"Serving with integrity"},{content:"Conducting ourselves and our business in an honest, ethical, and trustworthy manner"},{content:"Providing financial stewardship"},{content:"Growing through innovation and creativity"}],t.value={title:"Your Trusted Partner in Real Estate",content:`
+                <div class='flex flex-col gap-4 text-lg'>
+                    <div>
+                        Amber Realty is a licensed real estate agent registered with
+                        the Board of Valuers, Appraisers and Estate Agents of
+                        Malaysia, and based in Kuala Lumpur the capital city of
+                        Malaysian. 
+                    </div>
+                    <div>
+                        Amber Realty has always been committed to
+                        providing the highest standards in the practice of Real
+                        Estate and unwavering dedication to the satisfaction of
+                        customers. Amber Realty place equal significance on
+                        personalized service and continuing innovation in building
+                        lifetime relationships with our customers and communities.
+                    </div>
+                    <div>
+                        Our Team of highly motivated and experienced negotiators is 
+                        dedicated and committed to providing assistance to all real estate 
+                        investment needs and complete transactions in the most efficient manner. 
+                    </div>
+                    <div>
+                        Among the properties we handle are Residential, Commercial and Industrial, 
+                        including Land for various types of use. 
+                        We also undertake all categories of Projects Sales, on behalf of Developers. 
+                    </div>
+                    <div>
+                        With many years of experience in this field Amber Realty is 
+                        in a position to share feedbacks on requirements by the end-users.  
+                        This allows us, when called upon by developers, to advise them with the planning of the layout, 
+                        floor plans and the selling price, in accordance with the specific locations of the development.
+                    </div>
+                </div>`},{aboutUsContentOne:t,aboutUsContentTwo:a}}},B={class:"bg-primary","data-aos":"fade-up",style:{"margin-top":"3.5rem"}},C={class:"container mx-auto text-white h-[45vh] flex justify-center items-center"},q={class:"text-center flex flex-col gap-2"},L=e("div",{class:"text-5xl"},"About Us",-1),T={class:"row container mx-auto py-24 px-12 justify-between gap-20"},U={class:"col-12 col-lg-6 text-4xl font-bold leading-tight q-gutter-y-md"},$={"data-aos":"flip-up","data-aos-duration":"1200"},j=["innerHTML"],R={class:"col-12 col-lg-6 row gap-4","data-aos":"fade-up"},k=e("div",{class:"text-4xl font-bold"},"Core Values",-1),E=e("div",{class:"text-lg"},"We Believe In",-1),Q=[k,E],z={class:"col-12 col-lg-6"},M={"data-aos":"flip-up","data-aos-duration":"1200"};function O(t,a,V,n,W,H){const m=y("BaseLayout");return i(),b(m,null,{content:d(()=>[e("div",B,[e("div",C,[e("div",q,[L,e("div",null,[s(f,{"active-color":"secondary",class:"flex justify-center"},{default:d(()=>[s(l,{label:"Home",icon:"home",to:"/"}),s(l,{label:"About Us"})]),_:1})])])])]),e("div",T,[e("div",{class:o(["col-12 row",t.$q.screen.lt.lg?"gap-4":""])},[e("div",U,[e("div",{class:o(t.$q.screen.lt.lg?"text-center":"")},u(n.aboutUsContentOne.title),3),e("div",$,[s(r,{src:"images/about_us_one.jpg",fit:"contain",class:"rounded-lg"})])]),e("div",{class:o(["col-12 col-lg-6",t.$q.screen.lt.md?"":"px-8"]),innerHTML:n.aboutUsContentOne.content},null,10,j)],2),e("div",{class:o(["col-12 row",t.$q.screen.lt.lg?"gap-4":""])},[e("div",R,[e("div",{class:o(["col-12 flex flex-col gap-2",t.$q.screen.lt.lg?"text-center":""])},Q,2),(i(!0),p(w,null,x(n.aboutUsContentTwo,(g,h)=>(i(),p("div",{class:"col-12 col-lg-10 row",key:h},[e("div",{class:o(["col-12 border text-lg py-4 px-6 bg-secondary rounded-lg font-bold capitalize flex items-center",t.$q.screen.lt.lg?"justify-center text-center":""]),style:{}},u(g.content),3)]))),128))]),e("div",z,[e("div",M,[s(r,{src:"images/about_us_two.jpg",fit:"contain",class:"rounded-lg"})])])],2)])]),_:1})}const ae=_(A,[["render",O]]);export{ae as default};
