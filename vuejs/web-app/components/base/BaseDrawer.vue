5836dcdf8ae8abc37fbb5843c789cd2aadc54b25
ahjohn019 <yewrui@hotmail.com>
2024-05-29 14:57:15 +0800

    26052024
    - Change sidebar to overlay mode (mobile)
    - Fix auth web api return correct message if hit error

--- vuejs/web-app/components/base/BaseDrawer.vue
+++ vuejs/web-app/components/base/BaseDrawer.vue
@@ -1,43 +1,56 @@
+<script setup>
+import { ref } from 'vue';
+
+const baseDrawerMenu = ref({});
+const toggleDrawer = ref(false);
+
+baseDrawerMenu.value = [
+    { title: 'Home', value: 'home', route: '/' },
+    { title: 'About Us', value: 'about_us', route: '/about-us' },
+];
+
+const toggleDrawerMobile = () => {
+    toggleDrawer.value = !toggleDrawer.value;
+};
+</script>
+
 <template>
-    <div class="q-pa-md">
-        <q-btn-dropdown color="primary" dropdown-icon="menu" unelevated>
+    <q-toolbar class="justify-end">
+        <q-btn flat @click="toggleDrawerMobile" round dense icon="menu" />
+    </q-toolbar>
+
+    <q-drawer
+        v-model="toggleDrawer"
+        :width="200"
+        :breakpoint="768"
+        overlay
+        bordered
+        side="right"
+        :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+    >
+        <q-scroll-area class="fit">
             <q-list>
+                <q-item clickable v-close-popup>
+                    <q-item-section clickable @click="toggleDrawerMobile()">
+                        <q-item-label>
+                            <q-icon name="close" size="24px"
+                        /></q-item-label>
+                    </q-item-section>
+                </q-item>
                 <q-item
                     v-for="(menu, idx) in baseDrawerMenu"
                     :key="idx"
                     clickable
                     v-close-popup
+                    class="font-bold"
                 >
-                    <q-item-section clickable @click="onItemClick(menu.route)">
-                        <q-item-label>{{ menu.title }}</q-item-label>
+                    <q-item-section clickable>
+                        <router-link :to="menu.route" activeClass="text-red">
+                            <q-item-label>{{ menu.title }}</q-item-label>
+                        </router-link>
                     </q-item-section>
                 </q-item>
             </q-list>
-        </q-btn-dropdown>
-    </div>
+        </q-scroll-area>
+    </q-drawer>
 </template>
-
-<script>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-
-export default {
-    setup() {
-        const router = useRouter();
-        const baseDrawerMenu = ref({});
-
-        baseDrawerMenu.value = [
-            { title: 'Home', value: 'home', route: '/' },
-            { title: 'About Us', value: 'about_us', route: '/about-us' },
-            // { title: 'Property', value: 'property', route: '/property' },
-        ];
-
-        return {
-            onItemClick(routeName) {
-                router.push(routeName);
-            },
-            baseDrawerMenu,
-        };
-    },
-};
-</script>
