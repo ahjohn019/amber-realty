87d649ab54e6448aa3401bfce37111c802953f26
ahjohn019 <yewrui@hotmail.com>
2024-07-21 21:52:24 +0800

    feat: add auth for mobile view

--- vuejs/web-app/components/base/BaseDrawerAuth.vue
+++ vuejs/web-app/components/base/BaseDrawerAuth.vue
@@ -1,13 +1,109 @@
 <script setup>
 import { ref } from 'vue';
+import BaseAuthLogin from './BaseAuthLogin.vue';
+import BaseAuthSignUp from './BaseAuthSignUp.vue';
+import { useRouter } from 'vue-router';
+import { usePropertyAuthWebStore } from '@store_web/auth/index.js';
 
 const alert = ref(false);
 const maximizedToggle = ref(true);
+const loginClass = ref(true);
+const signUpClass = ref(false);
+const modalInfo = ref(true);
+const router = useRouter();
+const authCheck = ref(false);
+const webProperty = usePropertyAuthWebStore();
+const getAuthToken = webProperty.fetchSessionToken();
+
+const handleAuthComponent = (value) => {
+    switch (value) {
+        case 'signup':
+            loginClass.value = false;
+            signUpClass.value = true;
+            break;
+
+        default:
+            loginClass.value = true;
+            signUpClass.value = false;
+            break;
+    }
+};
+
+const handleInfoMessage = (value) => {
+    const responses = {
+        login: {
+            text: 'Login Successfully',
+            icon: 'success',
+        },
+        signup: {
+            text: 'Sign Up Successfully',
+            icon: 'success',
+        },
+    };
+    const response = responses[value.type];
+
+    if (response && value.message === 'success') {
+        modalInfo.value = false;
+        router.go(0);
+    }
+};
+
+const fetchProfile = async () => {
+    try {
+        const response = await webProperty.fetchProfile(getAuthToken);
+
+        if (response) {
+            authCheck.value = true;
+        }
+
+        console.log('response', response);
+
+        return response;
+    } catch (error) {}
+};
+
+const handleLogout = async () => {
+    try {
+        const response = await webProperty.handleLogoutProcess(getAuthToken);
+
+        if (response) {
+            authCheck.value = false;
+        }
+
+        return response;
+    } catch (error) {}
+};
+
+fetchProfile();
 </script>
 <template>
-    <q-item clickable v-close-popup class="font-bold" @click="alert = true">
+    <q-item
+        clickable
+        v-close-popup
+        class="font-bold"
+        @click="alert = true"
+        v-if="!authCheck"
+    >
         <q-item-section clickable>
             <q-item-label>Login</q-item-label>
+        </q-item-section>
+    </q-item>
+    <q-item clickable v-close-popup class="font-bold" v-if="authCheck">
+        <q-item-section clickable>
+            <router-link to="/profile" activeClass="text-red">
+                <q-item-label>Profile</q-item-label>
+            </router-link>
+        </q-item-section>
+    </q-item>
+    <q-item
+        clickable
+        v-close-popup
+        class="font-bold"
+        @click="handleLogout"
+        v-if="authCheck"
+    >
+        <q-item-section clickable>
+            <q-item-label>Logout</q-item-label>
         </q-item-section>
     </q-item>
     <q-dialog v-model="alert" :maximized="maximizedToggle">
@@ -46,15 +142,20 @@ const maximizedToggle = ref(true);
                 </q-btn>
             </q-bar>
 
-            <q-card-section>
-                <div class="text-h6">Alert</div>
+            <q-card-section class="q-pt-none" v-if="loginClass">
+                <BaseAuthLogin
+                    class="flex flex-col text-center justify-center h-[94vh]"
+                    @signUpSelection="handleAuthComponent"
+                    @infoMessage="handleInfoMessage"
+                />
             </q-card-section>
 
-            <q-card-section class="q-pt-none">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-                repellendus sit voluptate voluptas eveniet porro. Rerum
-                blanditiis perferendis totam, ea at omnis vel numquam
-                exercitationem aut, natus minima, porro labore.
+            <q-card-section class="q-pt-none" v-if="signUpClass">
+                <BaseAuthSignUp
+                    class="flex flex-col text-center justify-center h-[94vh]"
+                    @loginSelection="handleAuthComponent"
+                    @infoMessage="handleInfoMessage"
+                />
             </q-card-section>
         </q-card>
     </q-dialog>
