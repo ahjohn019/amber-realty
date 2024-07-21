fc9ceaa272bd3c70c88ec4b4fbf9056506301660
ahjohn019 <yewrui@hotmail.com>
2024-07-21 21:55:37 +0800

    #npm run prod

---
+++ public/build/assets/AboutUs-1e668214.js
@@ -0,0 +1,33 @@
+import{Q as l,a as g}from"./QBreadcrumbs-047be42b.js";import{Q as r}from"./QImg-ea549f2b.js";import{r as c,q as i,s as v,t as d,B as e,E as s,T as a,H as u,y as p,P as f,I as _}from"./ckeditor-75d48d99.js";import{_ as y}from"./BaseLayout-b328a276.js";import"./vm-287ad375.js";import"./QBtn-c4aed158.js";import"./QAvatar-e0d3e302.js";import"./QMenu-babb46b9.js";import"./use-tick-309b3908.js";import"./QScrollArea-2911723b.js";import"./QSeparator-a24a473f.js";import"./ClosePopup-188a1a79.js";import"./QSpace-f4e3315c.js";import"./QCardSection-23243dbf.js";import"./QCard-48064bf2.js";import"./QDialog-aaa61d1c.js";import"./QInput-2db4e532.js";import"./app-15662a92.js";import"./dayjs.min-d0ab8850.js";const b={class:"bg-primary","data-aos":"fade-up"},x={class:"container mx-auto text-white h-[45vh] flex justify-center items-center"},w={class:"text-center flex flex-col gap-2"},q=e("div",{class:"text-5xl"},"About Us",-1),A={class:"row container mx-auto py-24 px-12 justify-between gap-20"},B={class:"col-12 col-lg-6 text-4xl font-bold leading-tight q-gutter-y-md"},j={"data-aos":"flip-up","data-aos-duration":"1200"},C=["innerHTML"],R={class:"col-12 col-lg-6 row gap-4","data-aos":"fade-up"},T=e("div",{class:"text-4xl font-bold"},"Core Values",-1),$=e("div",{class:"text-lg"},"We Believe In",-1),k=[T,$],E={class:"col-12 col-lg-6"},L={"data-aos":"flip-up","data-aos-duration":"1200"},te={__name:"AboutUs",setup(Q){const o=c({}),n=c([]);return n.value=[{content:"Serving with integrity"},{content:"Conducting ourselves and our business in an honest, ethical, and trustworthy manner"},{content:"Providing financial stewardship"},{content:"Growing through innovation and creativity"}],o.value={title:"Your Trusted Partner in Real Estate",content:`
+                <div class='flex flex-col gap-4 text-lg'>
+                    <div>
+                        Amber Realty is a licensed real estate agent registered with
+                        the Board of Valuers, Appraisers and Estate Agents of
+                        Malaysia, and based in Kuala Lumpur the capital city of
+                        Malaysian. 
+                    </div>
+                    <div>
+                        Amber Realty has always been committed to
+                        providing the highest standards in the practice of Real
+                        Estate and unwavering dedication to the satisfaction of
+                        customers. Amber Realty place equal significance on
+                        personalized service and continuing innovation in building
+                        lifetime relationships with our customers and communities.
+                    </div>
+                    <div>
+                        Our Team of highly motivated and experienced negotiators is 
+                        dedicated and committed to providing assistance to all real estate 
+                        investment needs and complete transactions in the most efficient manner. 
+                    </div>
+                    <div>
+                        Among the properties we handle are Residential, Commercial and Industrial, 
+                        including Land for various types of use. 
+                        We also undertake all categories of Projects Sales, on behalf of Developers. 
+                    </div>
+                    <div>
+                        With many years of experience in this field Amber Realty is 
+                        in a position to share feedbacks on requirements by the end-users.  
+                        This allows us, when called upon by developers, to advise them with the planning of the layout, 
+                        floor plans and the selling price, in accordance with the specific locations of the development.
+                    </div>
+                </div>`},(t,U)=>(i(),v(y,null,{content:d(()=>[e("div",b,[e("div",x,[e("div",w,[q,e("div",null,[s(g,{"active-color":"secondary",class:"flex justify-center"},{default:d(()=>[s(l,{label:"Home",icon:"home",to:"/"}),s(l,{label:"About Us"})]),_:1})])])])]),e("div",A,[e("div",{class:a(["col-12 row",t.$q.screen.lt.lg?"gap-4":""])},[e("div",B,[e("div",{class:a(t.$q.screen.lt.lg?"text-center":"")},u(o.value.title),3),e("div",j,[s(r,{src:"images/about_us_one.jpg",fit:"contain",class:"rounded-lg"})])]),e("div",{class:a(["col-12 col-lg-6",t.$q.screen.lt.md?"":"px-8"]),innerHTML:o.value.content},null,10,C)],2),e("div",{class:a(["col-12 row",t.$q.screen.lt.lg?"gap-4":""])},[e("div",R,[e("div",{class:a(["col-12 flex flex-col gap-2",t.$q.screen.lt.lg?"text-center":""])},k,2),(i(!0),p(_,null,f(n.value,(m,h)=>(i(),p("div",{class:"col-12 col-lg-10 row",key:h},[e("div",{class:a(["col-12 border text-lg py-4 px-6 bg-secondary rounded-lg font-bold capitalize flex items-center",t.$q.screen.lt.lg?"justify-center text-center":""]),style:{}},u(m.content),3)]))),128))]),e("div",E,[e("div",L,[s(r,{src:"images/about_us_two.jpg",fit:"contain",class:"rounded-lg"})])])],2)])]),_:1}))}};export{te as default};
